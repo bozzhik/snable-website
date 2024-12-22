a8a9057fd0dfb83064402a83ce24aa0cc744084a
maxim <bozzhik@ya.ru>
2024-12-23 01:35:37 +0300

    fill `content` markdown for `/research`

--- src/components/UI/MDX.tsx
+++ src/components/UI/MDX.tsx
@@ -1,7 +1,21 @@
+import {ComponentPropsWithoutRef} from 'react'
 import {cn} from '@/lib/utils'
 
 export const MDX = {
   h1: ({className, ...props}: React.HTMLAttributes<HTMLHeadingElement>) => <h1 className={cn('mb-8 text-3xl text-neutral-500 font-semibold tracking-tighter', className)} {...props} />,
-  h2: ({className, ...props}: React.HTMLAttributes<HTMLHeadingElement>) => <h2 className={cn('mt-8 text-neutral-500 mb-4 text-3xl font-semibold tracking-tight', className)} {...props} />,
+  h2: ({className, ...props}: React.HTMLAttributes<HTMLHeadingElement>) => <h2 className={cn('uppercase mt-8 text-neutral-500 mb-4 text-3xl font-semibold tracking-tight', className)} {...props} />,
+  h3: ({className, ...props}: React.HTMLAttributes<HTMLHeadingElement>) => <h2 className={cn('uppercase mt-8 text-neutral-500 mb-4 text-2xl font-semibold tracking-[-0.015em]', className)} {...props} />,
   p: ({className, ...props}: React.HTMLAttributes<HTMLParagraphElement>) => <p className={cn('[&:not(:first-child)]:mt-5', className)} {...props} />,
+  em: ({className, ...props}: React.HTMLAttributes<HTMLSpanElement>) => <em className={cn('text-neutral-500 not-italic font-mono', className)} {...props} />,
+
+  // eslint-disable-next-line @next/next/no-img-element
+  img: ({className, ...props}: React.HTMLAttributes<HTMLImageElement>) => <img className={cn('pb-0.5', className)} {...props} alt="Snable research image" />,
+  a: ({href, children, ...props}: ComponentPropsWithoutRef<'a'>) => {
+    const className = 'pb-[1px] duration-200 border-b border-neutral-500 hover:border-transparent'
+    return (
+      <a href={href} target="_blank" rel="noopener noreferrer" className={className} {...props}>
+        {children}
+      </a>
+    )
+  },
 }
