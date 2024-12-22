fafdd9ab2a6bb96bd9ce3eca5d3023c96a7807f8
maxim <bozzhik@ya.ru>
2024-12-22 23:44:32 +0300

    init `/research` page

---
+++ src/app/research/page.tsx
@@ -0,0 +1,14 @@
+export default async function ResearchPage() {
+  return (
+    <div className="max-w-2xl py-8 mx-auto space-y-14">
+      <div className="space-y-2">
+        <h1 className="text-4xl font-semibold uppercase">Page Inspector Extension</h1>
+        <h2>Snable Extension</h2>
+      </div>
+
+      <main>
+        <p>my research paper</p>
+      </main>
+    </div>
+  )
+}
