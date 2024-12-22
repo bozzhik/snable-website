170069a7d7a83e893e95ccb7dbcb58b9fcdda298
maxim <bozzhik@ya.ru>
2024-12-22 23:32:23 +0300

    ы

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,9 +1,20 @@
-import ResearchBody from './ResearchBody'
+import path from 'path'
+import fs from 'fs/promises'
 
-export default function Home() {
+import {MDXRemote} from 'next-mdx-remote/rsc'
+import components from './components'
+
+async function getContent() {
+  const filePath = path.join(process.cwd(), 'src/app/example/content.mdx')
+  return await fs.readFile(filePath, 'utf8')
+}
+
+export default async function Home() {
+  const content = await getContent()
+
   return (
     <div className="container px-4 py-8 mx-auto max-w-none">
-      <ResearchBody />
+      <MDXRemote source={content} components={components} />
     </div>
   )
 }
