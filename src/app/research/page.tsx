17ae0bee5a22c53e62d9710e942f1bc322de89ed
maxim <bozzhik@ya.ru>
2024-12-23 05:12:57 +0300

    [3] fill `content` markdown for `/research`

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -13,7 +13,7 @@ export default async function ResearchPage() {
   const content = await getContent()
 
   return (
-    <div className="max-w-2xl py-8 mx-auto space-y-14">
+    <div className="max-w-2xl pt-8 pb-16 mx-auto sm:mx-3 sm:max-w-none space-y-14">
       <div className="space-y-1.5">
         <h1 className="text-3xl font-semibold tracking-tight uppercase">Snable Extension</h1>
         <div className="px-1 text-sm text-white rounded-sm bg-neutral-700 w-fit">Research</div>
