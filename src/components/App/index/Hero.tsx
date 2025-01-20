38988f99ac15329c9972be4bc271ba891aa962e0
maxim <bozzhik@ya.ru>
2025-01-20 23:46:09 +0300

    init `Typography` UI component (dynamic)

--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -1,17 +1,24 @@
 import heroImage from '$/hero.png'
 
 import Image from 'next/image'
+import {H1, P} from '~/UI/Typography'
+
+const heroContent = {
+  title: 'Snable Extension',
+  subtitle: 'Chrome extension that simplifies the process of collecting visual elements from websites',
+  image: heroImage,
+}
 
 export default function Hero() {
   return (
     <section data-section="hero-index" className="space-y-12">
       <div className="flex flex-col gap-3 items-center text-center">
-        <h1 className="text-gray text-[80px] leading-none font-semibold tracking-tighter">Snable Extension</h1>
-        <p className="font-mono uppercase text-white-dirty max-w-[50ch]">Chrome extension that simplifies the process of collecting visual elements from websites.</p>
+        <H1>{heroContent.title}</H1>
+        <P className="max-w-[50ch]">{heroContent.subtitle}</P>
       </div>
 
       <div className="group bg-black-light border-2 border-gray-dark rounded-[20px]">
-        <Image priority={true} quality={100} className="size-full object-cover group-hover:scale-[1.02] duration-500" src={heroImage} alt="Snable Chrome Extension overview" />
+        <Image priority={true} quality={100} className="group-hover:scale-[1.02] duration-500" src={heroImage} alt="Snable Chrome Extension overview" />
       </div>
     </section>
   )
---
+++ src/components/UI/Typography.tsx
@@ -0,0 +1,32 @@
+import {cn, m} from '@/lib/utils'
+
+type Props = {
+  type: TypoTypes
+  className?: string
+  children: React.ReactNode
+}
+
+export type TypoTypes = keyof typeof typoClasses
+
+export const typoClasses = {
+  h1: m('text-[80px] leading-none font-semibold tracking-tighter', 'text-gray'),
+  p: m('text-base uppercase', 'font-mono text-white-dirty'),
+} as const
+
+function Typography({type, className, children}: Props) {
+  const Element = type
+  return <Element className={cn(typoClasses[type], className)}>{children}</Element>
+}
+
+function createTypography(type: TypoTypes) {
+  const Component = ({className, children}: Omit<Props, 'type'>) => (
+    <Typography type={type} className={className}>
+      {children}
+    </Typography>
+  )
+  Component.displayName = `Typography(${type.toUpperCase()})`
+  return Component
+}
+
+export const H1 = createTypography('h1')
+export const P = createTypography('p')
