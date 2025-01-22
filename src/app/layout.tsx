b41d560601f4e5ba5d16ec41c9ff2676ec432c08
maxim <bozzhik@ya.ru>
2025-01-22 17:19:17 +0300

    add `Analytics` component based on Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ export {metadata} from '@/lib/layout-config'
 import {geistMono, suisseIntl} from '@/lib/layout-config'
 import '@/app/globals.css'
 
+import {Analytics as VercelAnalytics} from '@vercel/analytics/react'
+
 import YandexMetrika from '~/Global/Analytics'
 import Header from '~/Global/Header'
 import {Toaster} from '~/UI/Sonner'
@@ -18,7 +20,11 @@ export default function RootLayout({
         {children}
 
         <Toaster richColors />
-        {process.env.NODE_ENV === 'production' && <YandexMetrika />}
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <YandexMetrika /> <VercelAnalytics />
+          </>
+        )}
       </body>
     </html>
   )
