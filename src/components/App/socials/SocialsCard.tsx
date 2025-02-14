698aa47a93b6ca9dd2fb2d15b87df2dfe3f1743e
maxim <bozzhik@ya.ru>
2025-02-14 13:49:37 +0300

    finalize `SocialsCard` socials component

--- src/components/App/socials/SocialsCard.tsx
+++ src/components/App/socials/SocialsCard.tsx
@@ -1,4 +1,4 @@
-import {type SocialsItem} from '@/app/socials/storage'
+import {SocialSource, type SocialsItem} from '@/app/socials/storage'
 import {BUTTON_VARIANTS} from '~/UI/Button'
 
 import {cn} from '@/lib/utils'
@@ -6,13 +6,18 @@ import {cn} from '@/lib/utils'
 import Link from 'next/link'
 import Image from 'next/image'
 import {H2, P} from '~/UI/Typography'
+import SocialsIcon from './SocialsIcon'
 
 export default function SocialsCard({item}: {item: SocialsItem}) {
   const {source, link, title, content, image, video} = item
 
   return (
-    <Link href={link} className={cn(BUTTON_VARIANTS.outline, 'block p-5 sm:p-4 space-y-3.5 group', 'rounded-lg duration-300 bg-black-card hover:bg-gray-dark', 'border border-white/20 ring-[0.5px] ring-white/20')} target="_blank">
-      <div className="font-mono text-sm sm:text-base font-medium uppercase text-gray">{source}</div>
+    <Link href={link} className={cn(BUTTON_VARIANTS.outline, 'block p-5 xl:p-4 sm:p-3.5 space-y-3.5 group', 'rounded-lg duration-300 bg-black-card hover:bg-gray-dark', 'border border-white/20 ring-[0.5px] ring-white/20')} target="_blank">
+      <div className="flex gap-1.5">
+        <SocialsIcon mode="light" source={source as SocialSource} />
+
+        <div className="font-mono text-sm sm:text-base font-medium uppercase text-gray">{source !== 'x' ? source : undefined}</div>
+      </div>
 
       <div className="space-y-4">
         {(image || video) && (
--- src/components/App/socials/SocialsIcon.tsx
+++ src/components/App/socials/SocialsIcon.tsx
@@ -1,22 +1,32 @@
 import XIcon from '$/socials/x.svg'
+import XLightIcon from '$/socials/x-light.svg'
 import MediumIcon from '$/socials/medium.svg'
+import MediumLightIcon from '$/socials/medium-light.svg'
 import TelegramIcon from '$/socials/telegram.svg'
+import TelegramLightIcon from '$/socials/telegram-light.svg'
 import HabrIcon from '$/socials/habr.svg'
+import HabrLightIcon from '$/socials/habr-light.svg'
 
 import type {SocialSource} from '@/app/socials/storage'
 
 import {cn} from '@/lib/utils'
 import Image, {type StaticImageData} from 'next/image'
 
-const icons: Record<SocialSource, StaticImageData> = {
-  medium: MediumIcon,
-  telegram: TelegramIcon,
-  habr: HabrIcon,
-  x: XIcon,
+const icons: Record<SocialSource, {dark: StaticImageData; light: StaticImageData}> = {
+  medium: {dark: MediumIcon, light: MediumLightIcon},
+  telegram: {dark: TelegramIcon, light: TelegramLightIcon},
+  habr: {dark: HabrIcon, light: HabrLightIcon},
+  x: {dark: XIcon, light: XLightIcon},
 }
 
-export default function SocialsIcon({source, className}: {source: SocialSource; className?: string}) {
-  const icon = icons[source]
+type Props = {
+  source: SocialSource
+  mode?: 'dark' | 'light'
+  className?: string
+}
+
+export default function SocialsIcon({source, mode = 'dark', className}: Props) {
+  const icon = icons[source]?.[mode]
   if (!icon) return null
 
   return (
