4a6a249e088e2ba8d8be3eeb5479018d15cacbf1
maxim <bozzhik@ya.ru>
2025-03-10 02:54:25 +0300

    stylize `Marque` inside of `Snabled` index block

--- src/components/App/index/Snabled.tsx
+++ src/components/App/index/Snabled.tsx
@@ -4,7 +4,7 @@ import type {PostgrestError} from '@supabase/supabase-js'
 import {supabase} from '@/lib/supabase'
 import {cn, cleanUrl} from '@/lib/utils'
 
-import {H5, P} from '~/UI/Typography'
+import {H4, H5, P} from '~/UI/Typography'
 import {Marquee} from '~/Modules/Marque'
 
 export default async function Snabled() {
@@ -20,28 +20,37 @@ export default async function Snabled() {
   }
 
   return (
-    <section data-section="snabled-index" className={cn('mt-24')}>
+    <section data-section="snabled-index" className={cn('mt-24', 'space-y-6')}>
+      <H4 className="text-center">Recent Sessions</H4>
+
       <div className="relative flex flex-col items-center justify-center w-full overflow-hidden">
         <Marquee pauseOnHover className="[--duration:20s] [--gap:0.75rem]">
-          {sessions.map((tab) => (
-            <a href={tab.url} className={cn('min-w-64', 'group p-2 pr-2.5 flex items-center gap-2.5', 'bg-black-light border border-gray-medium rounded-xl', 'duration-300 hover:bg-black hover:border-gray-medium/70')} target="_blank" rel="noopener noreferrer" key={tab.url}>
-              {tab.favicon && (
-                <div className={cn('size-11 grid place-items-center rounded-lg overflow-hidden', 'relative flex-shrink-0')}>
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img className="object-contain object-center rounded-sm" src={tab.favicon} alt={tab.title} />
-                </div>
-              )}
+          {sessions.map((tab) => {
+            const {favicon, url, title} = tab
+            if (!favicon) return null
 
-              <div className="flex-grow min-w-0">
-                <H5 className="mt-0.5 leading-[1.2] duration-300 group-hover:text-gray line-clamp-1">{tab.title.slice(0, 20)}</H5>
-                <P className="font-mono text-xs truncate text-gray">{cleanUrl(tab.url.slice(0, 40))}</P>
-              </div>
-            </a>
-          ))}
+            const textGradient = 'bg-gradient-to-r to-black/0 bg-clip-text text-transparent'
+
+            return (
+              <a href={url} className={cn('min-w-64', 'group p-2 pr-2.5 flex items-center gap-2.5', 'bg-black-light border border-gray-medium rounded-xl', 'duration-300 hover:bg-black hover:border-gray-medium/70')} target="_blank" rel="noopener noreferrer" key={url}>
+                {favicon && (
+                  <div className={cn('size-11 grid place-items-center rounded-lg overflow-hidden', 'relative flex-shrink-0')}>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className="object-contain object-center rounded-sm" src={favicon} alt={title} />
+                  </div>
+                )}
+
+                <div className="flex-grow min-w-0">
+                  <H5 className={cn('mt-0.5 leading-[1.2] duration-300 group-hover:text-gray line-clamp-1', title.length > 20 && `from-white-dirty via-white-dirty ${textGradient}`)}>{title.slice(0, 20)}</H5>
+                  <P className={cn('font-mono text-xs truncate text-gray', url.length > 40 && `from-gray via-gray ${textGradient}`)}>{cleanUrl(url.slice(0, 40))}</P>
+                </div>
+              </a>
+            )
+          })}
         </Marquee>
 
-        <div className="absolute inset-y-0 left-0 w-[25%] pointer-events-none bg-gradient-to-r from-black via-gray-dark/40"></div>
-        <div className="absolute inset-y-0 right-0 w-[25%] pointer-events-none bg-gradient-to-l from-black via-gray-dark/40"></div>
+        <div className="absolute inset-y-0 left-0 w-[15%] pointer-events-none bg-gradient-to-r from-black via-gray-dark/40"></div>
+        <div className="absolute inset-y-0 right-0 w-[15%] pointer-events-none bg-gradient-to-l from-black via-gray-dark/40"></div>
       </div>
     </section>
   )
