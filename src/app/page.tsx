99fc720d59043667b226ece2daa20e6a7869bcb4
maxim <bozzhik@ya.ru>
2025-03-10 00:26:41 +0300

    finalize `size` prop for `Button` UI component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Container from '~/Global/Container'
+
 import Hero from '~~/index/Hero'
 
 export default function IndexPage() {
   return (
-    <>
+    <Container variant="default" className="space-y-36 sm:space-y-44 sm:pt-0">
       <Hero />
-    </>
+    </Container>
   )
 }
--- src/components/App/index/Hero.tsx
+++ src/components/App/index/Hero.tsx
@@ -1,13 +1,14 @@
 import heroImage from '$/hero.png'
 
-import {BOX_STYLES} from '~/Global/Container'
+import {PROJECT_LINKS} from '@/lib/constants'
 import {cn} from '@/lib/utils'
 
 import Image from 'next/image'
 import {H1, H4} from '~/UI/Typography'
+import Button from '~/UI/Button'
+import {Chrome} from 'lucide-react'
 
-export const SCREEN_HEIGHT = 'sm:h-screen sm:h-svh!'
-const {box, padding} = BOX_STYLES.default
+export const MOB_SCREEN_HEIGHT = 'sm:h-screen sm:h-svh!'
 
 const heroContent = {
   title: 'Snable Extension',
@@ -17,10 +18,17 @@ const heroContent = {
 
 export default function Hero() {
   return (
-    <section data-section="hero-teaser" className={cn('flex flex-col gap-12 sm:gap-10', [box, padding], 'sm:pt-28 sm:pb-2', SCREEN_HEIGHT)}>
-      <div className={cn('relative z-[-20]', 'flex flex-col items-center gap-3 text-center xl:gap-4')}>
-        <H1>{heroContent.title}</H1>
-        <H4 className="max-w-[50ch]">{heroContent.subtitle}</H4>
+    <section data-section="hero-index" className={cn('flex flex-col gap-12 sm:gap-8', 'sm:pt-28 sm:pb-2.5', MOB_SCREEN_HEIGHT)}>
+      <div className="space-y-8 sm:space-y-4">
+        <div className={cn('relative z-[-20]', 'flex flex-col items-center gap-3 text-center xl:gap-4')}>
+          <H1>{heroContent.title}</H1>
+          <H4 className="max-w-[50ch]">{heroContent.subtitle}</H4>
+        </div>
+
+        <div className={cn('flex sm:flex-col justify-center gap-2')}>
+          <Button to={PROJECT_LINKS.store} className="sm:w-full" icon={<Chrome strokeWidth={1.5} />} text="Get for Chrome" />
+          <Button to="#features" className="sm:hidden" variant="outline" text="See Features" />
+        </div>
       </div>
 
       <div className="group bg-black-light border-2 border-gray-dark rounded-[20px] sm:rounded-[15px] overflow-hidden sm:h-full">
