a0d33211e4e7cff8d66bcf149f955ff86cf276a1
maxim <bozzhik@ya.ru>
2025-03-10 01:18:24 +0300

    power `Snabled` with `supabase` tracked `sessions`

--- src/components/App/index/Snabled.tsx
+++ src/components/App/index/Snabled.tsx
@@ -1,16 +1,37 @@
+import type {TabInfo} from '@/app/api/session/route'
+import type {PostgrestError} from '@supabase/supabase-js'
+
+import {supabase} from '@/lib/supabase'
 import {cn, cleanUrl} from '@/lib/utils'
+
 import {H5, P} from '~/UI/Typography'
 
-export default function Snabled() {
+export default async function Snabled() {
+  const {data: sessions, error} = (await supabase
+    .from('sessions') // tab data
+    .select('*')
+    .order('pin', {ascending: true}) // cool sessions
+    .order('created_at', {ascending: false})
+    .limit(15)) as {data: TabInfo[] | null; error: PostgrestError | null}
+
+  if (error || !sessions?.length) {
+    return null
+  }
+
   return (
     <section data-section="snabled-index" className={cn('mt-24', 'relative w-full flex gap-3 justify-center overflow-hidden')}>
-      {['example-1', 'example-2', 'example-3', 'example-4', 'example-5'].map((tab, idx) => (
-        <a href={tab} className={cn('group p-2 pr-2.5 flex items-center gap-2.5', 'bg-black-light border border-gray-medium rounded-xl', 'duration-300 hover:bg-black hover:border-gray-medium/70')} target="_blank" rel="noopener noreferrer" key={idx}>
-          <div className="relative flex-shrink-0 rounded-lg size-11 bg-gray"></div>
+      {sessions.map((tab) => (
+        <a href={tab.url} className={cn('min-w-52', 'group p-2 pr-2.5 flex items-center gap-2.5', 'bg-black-light border border-gray-medium rounded-xl', 'duration-300 hover:bg-black hover:border-gray-medium/70')} target="_blank" rel="noopener noreferrer" key={tab.url}>
+          {tab.favicon && (
+            <div className={cn('size-11 grid place-items-center rounded-lg overflow-hidden', 'relative flex-shrink-0')}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img className="object-contain object-center rounded-sm" src={tab.favicon} alt={tab.title} />
+            </div>
+          )}
 
-          <div className="flex-grow min-w-0 ">
-            <H5 className="leading-[1.25] duration-300 group-hover:text-gray">{tab}</H5>
-            <P className="font-mono text-xs truncate text-gray">{cleanUrl(`https://${tab}.com`)}</P>
+          <div className="flex-grow min-w-0">
+            <H5 className="leading-[1.25] duration-300 group-hover:text-gray line-clamp-1">{tab.title.slice(0, 15)}</H5>
+            <P className="font-mono text-xs truncate text-gray">{cleanUrl(tab.url.slice(0, 38))}</P>
           </div>
         </a>
       ))}
