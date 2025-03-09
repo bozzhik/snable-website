f9294ade1b219035896fd32a3976d5371deaed68
maxim <bozzhik@ya.ru>
2025-03-10 01:44:15 +0300

    init `Marque` Module component using `magicui`
    https://magicui.design/docs/components/marquee

--- src/components/App/index/Snabled.tsx
+++ src/components/App/index/Snabled.tsx
@@ -5,6 +5,7 @@ import {supabase} from '@/lib/supabase'
 import {cn, cleanUrl} from '@/lib/utils'
 
 import {H5, P} from '~/UI/Typography'
+import {Marquee} from '~/Modules/Marque'
 
 export default async function Snabled() {
   const {data: sessions, error} = (await supabase
@@ -19,22 +20,29 @@ export default async function Snabled() {
   }
 
   return (
-    <section data-section="snabled-index" className={cn('mt-24', 'relative w-full flex gap-3 justify-center overflow-hidden')}>
-      {sessions.map((tab) => (
-        <a href={tab.url} className={cn('min-w-52', 'group p-2 pr-2.5 flex items-center gap-2.5', 'bg-black-light border border-gray-medium rounded-xl', 'duration-300 hover:bg-black hover:border-gray-medium/70')} target="_blank" rel="noopener noreferrer" key={tab.url}>
-          {tab.favicon && (
-            <div className={cn('size-11 grid place-items-center rounded-lg overflow-hidden', 'relative flex-shrink-0')}>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img className="object-contain object-center rounded-sm" src={tab.favicon} alt={tab.title} />
-            </div>
-          )}
+    <section data-section="snabled-index" className={cn('mt-24')}>
+      <div className="relative flex flex-col items-center justify-center w-full overflow-hidden">
+        <Marquee pauseOnHover className="[--duration:20s] [--gap:0.75rem]">
+          {sessions.map((tab) => (
+            <a href={tab.url} className={cn('min-w-64', 'group p-2 pr-2.5 flex items-center gap-2.5', 'bg-black-light border border-gray-medium rounded-xl', 'duration-300 hover:bg-black hover:border-gray-medium/70')} target="_blank" rel="noopener noreferrer" key={tab.url}>
+              {tab.favicon && (
+                <div className={cn('size-11 grid place-items-center rounded-lg overflow-hidden', 'relative flex-shrink-0')}>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img className="object-contain object-center rounded-sm" src={tab.favicon} alt={tab.title} />
+                </div>
+              )}
 
-          <div className="flex-grow min-w-0">
-            <H5 className="leading-[1.25] duration-300 group-hover:text-gray line-clamp-1">{tab.title.slice(0, 15)}</H5>
-            <P className="font-mono text-xs truncate text-gray">{cleanUrl(tab.url.slice(0, 38))}</P>
-          </div>
-        </a>
-      ))}
+              <div className="flex-grow min-w-0">
+                <H5 className="mt-0.5 leading-[1.2] duration-300 group-hover:text-gray line-clamp-1">{tab.title.slice(0, 20)}</H5>
+                <P className="font-mono text-xs truncate text-gray">{cleanUrl(tab.url.slice(0, 40))}</P>
+              </div>
+            </a>
+          ))}
+        </Marquee>
+
+        <div className="absolute inset-y-0 left-0 w-[25%] pointer-events-none bg-gradient-to-r from-black via-gray-dark/40"></div>
+        <div className="absolute inset-y-0 right-0 w-[25%] pointer-events-none bg-gradient-to-l from-black via-gray-dark/40"></div>
+      </div>
     </section>
   )
 }
