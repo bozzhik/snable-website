978b6c5a0bdbd0d3eb169c7a7ee1f1900a891d8a
maxim <bozzhik@ya.ru>
2025-06-04 23:56:12 +0300

    restore `Launch` teaser block to `/index` page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import Container from '~/Global/Container'
 import Hero from '~~/index/Hero'
 import Snabled from '~~/index/Snabled'
 import Achievements from '~~/index/Achievements'
+import Teaser from '~~/teaser/Launch'
 
 export default function IndexPage() {
   return (
-    <Container variant="default" className="space-y-36 sm:pt-0">
+    <Container variant="default" className="space-y-36 pb-40 sm:pt-0">
       <Hero />
       <Snabled />
 
       <Achievements />
+      <Teaser />
     </Container>
   )
 }
--- src/components/App/teaser/Launch.tsx
+++ src/components/App/teaser/Launch.tsx
@@ -11,7 +11,7 @@ export default function Launch() {
   return (
     <section id="form" data-section="launch-teaser" className={cn('max-w-[29vw] xl:max-w-[34vw] sm:max-w-none mx-auto sm:px-2', 'flex flex-col items-center justify-center gap-3 xl:gap-2.5')}>
       <div className="space-y-3 sm:space-y-5 w-full">
-        <H4 className="text-lg text-center xl:text-base sm:px-14">Don&#39;t miss our launch! Get updates, no spam.</H4>
+        <H4 className="text-lg text-center xl:text-base sm:px-14">Don&#39;t miss our reveals! Get updates, no spam.</H4>
 
         <TeaserForm />
       </div>
